9ce8731ce8bdfcd4c755795663944a3bb9632b05
LaTrindade <lactrindade@yahoo.com.br>
2024-03-07 23:13:32 +0000

    botao copiar

--- codificador/funcoes.js
+++ codificador/funcoes.js
@@ -8,7 +8,7 @@ function criptografar() {
     texto = texto.replaceAll('e', 'enter');
     texto = texto.replaceAll('i', 'imes');
     texto = texto.replaceAll('a', 'ai');
-    texto = texto.replaceAll('o', 'obter');
+    texto = texto.replaceAll('o', 'ober');
     texto = texto.replaceAll('u', 'ufat');
 
     let pTexto = document.getElementById("pTexto");
@@ -25,7 +25,7 @@ function descriptografar() {
     let inpTexto = document.getElementById("inpTexto");
     let texto = inpTexto.value;
     texto = texto.replaceAll('ufat','u');
-    texto = texto.replaceAll('obter','o');
+    texto = texto.replaceAll('ober','o');
     texto = texto.replaceAll('ai','a' );
     texto = texto.replaceAll('imes','i');
     texto = texto.replaceAll('enter','e');
@@ -35,3 +35,13 @@ function descriptografar() {
     pTexto.textContent = texto;
 
 }
+
+function readText() {
+    let pTexto = document.getElementById("pTexto");
+    //let pTexto = document.querySelector("#pTexto");
+alert(pTexto.textContent);
+   // navigator.clipboard.readText(pTexto.textContent).then(() => {alert("Texto copiado: "+pTexto.textContent)});
+   navigator.clipboard.writeText(pTexto.textContent);
+}
+
+ 
